07e187455ee4742d16fb481c7d596776c9f3cc08
Joachim Dornbusch <joachim.dornbusch@agroparistech.fr>
2015-05-11 06:46:52 +0200

    Retrait de code lié à la sécurité.

--- src/fr/ac_versailles/crdp/apiscol/ParametersKeys.java
+++ src/fr/ac_versailles/crdp/apiscol/ParametersKeys.java
@@ -8,12 +8,7 @@ public enum ParametersKeys {
 			"apiscol_instance_name"), apiscolInstanceLabel(
 			"apiscol_instance_label"), solrAddress("solr_address"), solrSearchPath(
 			"solr_search_path"), solrUpdatePath("solr_update_path"), solrExtractPath(
-			"solr_extract_path"), solrSuggestPath("solr_suggest_path"), sharedSecret(
-			"shared_secret"), contentWsSharedSecret("content_ws_shared_secret"), metaWsSharedSecret(
-			"meta_ws_shared_secret"), packWsSharedSecret(
-			"pack_ws_shared_secret"), thumbsWsSharedSecret(
-			"thumbs_ws_shared_secret"), conversionWsSharedSecret(
-			"conversion_ws_shared_secret"), contentWebserviceUrl(
+			"solr_extract_path"), solrSuggestPath("solr_suggest_path"), contentWebserviceUrl(
 			"content_webservice_url"), metadataWebserviceUrl(
 			"metadata_webservice_url"), packWebserviceUrl("pack_webservice_url"), thumbsWebserviceUrl(
 			"thumbs_webservice_url"), conversionWebserviceUrl(
@@ -22,7 +17,8 @@ public enum ParametersKeys {
 			"absolute_page_limit"), apiscolVersion("apiscol_version"), cleaningDelay(
 			"cleaning_delay"), automaticThumbChoiceDelay(
 			"automatic_thumb_choice_delay"), automaticThumbChoiceMaxNumberOfTries(
-			"automatic_thumb_choice_max_number_of_tries"), logLevel("log_level"), epubPreviewQuality("epub_preview_quality");
+			"automatic_thumb_choice_max_number_of_tries"), logLevel("log_level"), epubPreviewQuality(
+			"epub_preview_quality");
 	private String value;
 
 	private ParametersKeys(String value) {
